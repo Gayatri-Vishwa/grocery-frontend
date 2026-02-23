45ea25da3aced253a89c67a9154b4df4c4318e86
gayatriDev02 <blossomjasmine02@gmail.com>
2026-02-23 10:51:45 +0530

    fix vercel routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,8 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative z-50">
+    {/* // <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all"> */}
       <Link className="text-2xl text-red-600 font-bold" to={"/"}>
         Grocery App
       </Link>
@@ -40,7 +41,7 @@ const Navbar = () => {
       <div className="hidden sm:flex items-center gap-8">
         <Link to={"/"}>Home</Link>
         <Link to={"/products"}>All Products</Link>
-        <Link to={"/seller"}>seller</Link>
+        <Link to={"/seller"}>seller login</Link>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
@@ -165,10 +166,15 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <div
+  className={`${
+    open ? "flex" : "hidden"
+  } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-50`}
+>
+      {/* <div
         className={`${
           open ? "flex" : "hidden"
         } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
-      >
+      > */}
         <a href="#" className="block">
           Home
         </a>
@@ -178,6 +184,7 @@ const Navbar = () => {
         <a href="#" className="block">
           Contact
         </a>
+        <a href="/seller">Seller Portal</a>
         {user ? (
           <>
             <div className="relative group">
