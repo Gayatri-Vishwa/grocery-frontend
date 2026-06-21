0c6e2e7fe9b67ee3d93be716b5b773af65421a4a
gayatriDev02 <blossomjasmine02@gmail.com>
2026-06-21 21:38:23 +0530

    add-product

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -49,13 +49,15 @@ useEffect(()=>{
                     <div className="flex flex-col gap-3">
                         {product.image?.map((image, index) => ( //for thumbnail array iteration
                             <div key={index} onClick={() => setThumbnail(image)} className="border max-w-24 border-gray-500/30 rounded overflow-hidden cursor-pointer" >
-                                <img src={`http://localhost:5000/images/${image}`} alt={`Thumbnail ${index + 1}`} />
+                                {/* <img src={`http://localhost:5000/images/${image}`} alt={`Thumbnail ${index + 1}`} /> */}
+                                <img src={image} alt={`Thumbnail ${index + 1}`} />
                             </div>
                         ))}
                     </div>
 
                     <div className="border border-gray-500/30 max-w-100 rounded overflow-hidden">
-                        <img src={`http://localhost:5000/images/${thumbnail}`} alt="Selected product" className="w-full h-full object-cover" />
+                        {/* <img src={`http://localhost:5000/images/${thumbnail}`} alt="Selected product" className="w-full h-full object-cover" /> */}
+                   <img src={thumbnail} alt="Selected product" className="w-full h-full object-cover" />
                     </div>
                 </div>
 
